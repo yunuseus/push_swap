ead8ed4b9c9c84d8dec2d933dbabebb30e231200
Yunus Emre Alp <yalp@k1m41s06.42istanbul.com.tr>
2025-02-10 18:09:47 +0300

    m

---
+++ extras.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   extras.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yalp <yalp@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 18:02:02 by yalp              #+#    #+#             */
+/*   Updated: 2025/02/10 18:06:48 by yalp             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	swap_two(t_nodes **lst)
+{
+	if ((*lst)->content > (*lst)->next->content)
+		sa(lst);
+}
+
+void	swap_three(t_nodes **lst)
+{
+
+}
+
+void	swap_four(t_nodes **lst)
+{
+	
+}
+
+void	swap_five(t_nodes **lst)
+{
+
+}
